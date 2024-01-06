9862bc77038d02cdfcc083eea502a88b9703f031
Faezeh-Karami <fk96karami@gmail.com>
2024-01-06 15:37:15 +0330

    create ToDoList app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ToDoList from "./component/toDoList/toDoList";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function App() {
+  return (
+    <>
+      <ToDoList />
+    </>
+  );
+}
